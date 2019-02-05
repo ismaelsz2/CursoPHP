6e1e8b89c2e5a495deeacf625a40ff0418d9d196
Ismael de Souza <ismaelsz2@live.com>
2019-02-05 00:09:24 -0300

    Aulas 15, 16 e 17 do curso de PHP

---
+++ exvariavel-04.php
@@ -0,0 +1,18 @@
+<?php 
+
+$nome = (int)$_GET["a"];
+
+//var_dump($nome);
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
+
+$ip1 = $_SERVER["SCRIPT_NAME"];
+
+echo $ip;
+
+echo "<br>";
+
+echo($ip1);
+
+ ?>
---
+++ exvariavel-05.php
@@ -0,0 +1,23 @@
+<?php 
+
+$nome = "Ismael";
+
+function teste(){
+
+	global $nome;
+	echo $nome;
+}
+
+
+
+function teste2(){
+	$nome = "joao";
+	echo $nome . " " . "Agora no teste2";
+
+}
+
+teste();
+echo "<br>";
+teste2();
+
+ ?>
