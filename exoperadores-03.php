515faf1800dbd0969058506b789c5ca06f06a11b
Ismael de Souza <ismaelsz2@live.com>
2019-02-07 00:14:22 -0300

    Arquivos da aula 17

---
+++ exoperadores-03.php
@@ -0,0 +1,34 @@
+<?php 
+
+$a = 10;
+
+$b = 2;
+
+echo $a + $b;
+
+echo "<br>";
+
+echo $a - $b;
+
+echo "<br>";
+
+echo $a * $b;
+
+echo "<br>";
+
+echo $a / $b;
+
+echo "<br>";
+
+echo $a % $b;
+
+echo "<br>";
+
+echo $a ** $b;
+
+
+
+
+
+
+ ?>
---
+++ exoperadores-08.php
@@ -0,0 +1,12 @@
+<?php 
+
+$resultado = (10 + 3) / 2 > 5 || 10 < 3;
+
+//echo $resultado;
+
+var_dump($resultado);
+
+
+
+
+ ?>
