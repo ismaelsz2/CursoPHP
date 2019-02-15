d71c9f97343c3feb97a50bec79f2011de9e80807
Ismael de Souza <ismaelsz2@live.com>
2019-02-14 23:23:33 -0300

    Implementação das aulas 20, 21 e 22

---
+++ exswitch-01.php
@@ -0,0 +1,54 @@
+<?php 
+
+$diaSemana = 6;//date(6);
+
+//echo $diaSemana;
+
+switch ($diaSemana) {
+		
+	
+		case 0:
+			echo "Domingo";
+
+			break;
+
+		case 1:
+			echo "Segunda-Feira";
+
+			break;
+
+		case 2:
+			echo "Terça-Feira";
+
+			break;
+
+		case 3:
+			echo "Quarta-Feira";
+
+			break;
+
+		case 4:
+			echo "Quinta-Feira";
+
+			break;
+
+		case 5:
+			echo "Sexta-Feira";
+
+			break;
+
+		case 6:
+			echo "Sábado";
+
+			break;
+
+		default:
+			echo "Data inválida!!!";
+			break;
+	
+	
+}
+
+
+
+ ?>
